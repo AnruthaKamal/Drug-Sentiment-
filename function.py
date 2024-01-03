50f3b34292c1e8357244019b55074ab7b722e0d4
Anrutha <107014168+AnruthaKamal@users.noreply.github.com>
2024-01-03 20:19:34 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,10 +1,6 @@
 import json
-import pandas as pd 
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS
 import nltk
 import string
 import re
